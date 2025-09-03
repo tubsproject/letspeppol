d06db1a7deb7c15d8c35544275835f01a4ae8dff
Michiel de Jong <michiel@pondersource.com>
2025-09-03 09:04:57 +0200

    Switch to JWT

--- proxy/src/db.ts
+++ proxy/src/db.ts
@@ -1,5 +1,6 @@
 import { createHash } from 'crypto';
 import { Client } from 'pg';
+import jsonwebtoken from 'jsonwebtoken';
 const client = await getPostgresClient();
 
 function sha256(plaintext: string): string {
@@ -42,22 +43,30 @@ WHERE peppolId = $1  AND passHash = $2
 }
 
 export async function generateToken(peppolId: string): Promise<string> {
-  const token = Math.random().toString(36).slice(2);
-  const query = `
-INSERT INTO sessions (peppolId, token, expires)
-VALUES ($1, $2, now()::timestamp + interval '1 day')
-`;
-  await client.query(query, [peppolId, token]);
+
+  const secretKey = 'yourSecretKey'; // Replace with your own secret key
+  const options = {
+    expiresIn: '1h', // Token expiration time
+  };
+  const payload = { peppolId };
+  const token = jsonwebtoken.sign(payload, secretKey, options);
+//   const query = `
+// INSERT INTO sessions (peppolId, token, expires)
+// VALUES ($1, $2, now()::timestamp + interval '1 day')
+// `;
+//   await client.query(query, [peppolId, token]);
   console.log(`Generated token for ${peppolId}: ${token}`);
   return token;
 }
 
 export async function checkBearerToken(token: string): Promise<string | null> {
-  const query = `
-SELECT peppolId
-FROM sessions
-WHERE token = $1 AND expires > now()
-`;
-  const result = await client.query(query, [token]);
-  return result.rows[0]?.peppolid || null;
+  return new Promise((resolve) => {
+    jsonwebtoken.verify(token, 'yourSecretKey', (err, payload) => {
+      if (err) {
+        resolve(null);
+      } else {
+        resolve(payload.peppolId);
+      }
+    });
+  });
 }
