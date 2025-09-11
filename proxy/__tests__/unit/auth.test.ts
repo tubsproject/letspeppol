5d042b015fe7c8b7acb65445cbf018c56d830877
Michiel de Jong <michiel@pondersource.com>
2025-09-11 11:38:07 +0200

    debugging the auth test

--- proxy/__tests__/unit/auth.test.ts
+++ proxy/__tests__/unit/auth.test.ts
@@ -5,16 +5,16 @@ import { checkBearerToken } from '../../src/auth.js';
 const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXBwb2xJZCI6InNvbWUtdXNlciIsImlhdCI6MTc1NzQzMDk4MCwiZXhwIjo1MzU3NDMwOTgwfQ.Zidxcwu343qBZRSRQddcTSCbNtRMo6rY2hLgCesinco`;
 
 // ACCESS_TOKEN_KEY='wrong' node token.js some-user
-const invalidToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXBwb2xJZCI6InNvbWUtdXNlciIsImlhdCI6MTc1NzMzMjY5NSwiZXhwIjo1MzU3MzMyNjk1fQ.8ONrzFsvwvq5Hgh5piDVJaCogyoTbv14KdrIQy3TEIE`;
+const invalidToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXBwb2xJZCI6InNvbWUtdXNlciIsImlhdCI6MTc1NzQzMzU0NywiZXhwIjo1MzU3NDMzNTQ3fQ.PPf-wMm0BAoctE25B9jw9m-NBpEI0BIMhLELcp0JQiQ`;
 
 describe('checkBearerToken function', () => {
   it('should return user id for valid token', async () => {
-    const result = await checkBearerToken(`Bearer ${token}`, 'secret-string');
+    const result = await checkBearerToken(token, 'secret-string');
     expect(result).toBe('some-user');
   });
 
   it('should throw for invalid token', async () => {
-   const promise = checkBearerToken(`Bearer ${invalidToken}`, 'wrong');
+   const promise = checkBearerToken(invalidToken, 'wrong');
    await expect(promise).rejects.toThrowError();
   });
 });
