53c6e6f904429c62e1dc6efa57d96794edd45661
Michiel de Jong <michiel@pondersource.com>
2025-09-30 10:21:28 +0200

    crowdfunding holding page

---
+++ docs/Let's Peppol _ Home_files/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-76b8c2c3276da9d6.js"],"/[[...slug]]":["static/chunks/pages/[[...slug]]-955358149bb8f9a3.js"],sortedPages:["/_app","/_error","/[[...slug]]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
